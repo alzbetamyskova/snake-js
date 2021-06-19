a0dbba7ae01ce5a1228d8fe4d2d717879277c85d
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-06-19 17:36:01 +0200

    jídlo pro hada  a kolize

--- main.js
+++ main.js
@@ -4,24 +4,36 @@ console.log('Funguju!');
 
 // canvas
 const canvas = document.querySelector('canvas');
+const title = document.querySelector('h1');
 const ctx = canvas.getContext("2d");
 
 // player
-let snakeSize = 50;
+let snakeSize = 30;
 
-let snakeSpeed = 1;
+let snakeSpeed = snakeSize;
 let snakePosX = 0;
-let snakePosY = canvas.height / 2 - snakeSize / 2;
+let snakePosY = canvas.height / 2;
 
 let velocityX = 0;
 let velocityY = 0;
+
+// food
+let foodPosX = 0;
+let foodPosY = 0;
+
+// game
+let score = 0;
+const fps = 15;
+
+const tileCountX = canvas.width / snakeSize;
+const tileCountY = canvas.height / snakeSize;
 
 // loop
 const gameLoop = () => {
 
   drawStuff();
   moveStuff();
-  requestAnimationFrame(gameLoop); 
+  setTimeout(gameLoop, 1000 / fps); 
 
 };
 
@@ -33,17 +45,24 @@ const moveStuff = () => {
   snakePosX += snakeSpeed * velocityX;
   snakePosY += snakeSpeed * velocityY;
 
-  if (snakePosX > canvas.width) {
+  // wall colision
+  if (snakePosX > canvas.width - snakeSize) {
     snakePosX = 0;
   };
   if (snakePosX < 0) {
     snakePosX = canvas.width;
   };
-  if (snakePosY > canvas.height) {
+  if (snakePosY > canvas.height - snakeSize) {
     snakePosY = 0;
   };
   if (snakePosY < 0) {
     snakePosY = canvas.height;
+  };
+
+  // food colision
+  if (snakePosX === foodPosX && snakePosY === foodPosY) {
+    title.textContent = ++score;
+    resetFood()
   };
 
 };
@@ -51,9 +70,13 @@ const moveStuff = () => {
 // ** DRAW EVERYTHING
 const drawStuff = () => {
 
-  rectangle('white', 0, 0, canvas.width, canvas.height);
+  rectangle('#EDEDED', 0, 0, canvas.width, canvas.height);
+
+  drawGrid()
+
+  rectangle('#a4da00', foodPosX, foodPosY, snakeSize, snakeSize)
   
-  rectangle('black', snakePosX, snakePosY, snakeSize, snakeSize);
+  rectangle('#DA0037', snakePosX, snakePosY, snakeSize, snakeSize);
 
 };
 
@@ -65,28 +88,58 @@ const rectangle = (color, x, y, width, height) => {
 
 };
 
+// draw grid
+const drawGrid = () => {
+
+  for ( let i = 0; i < tileCountX; i ++) {
+    for (let j = 0; j < tileCountY; j ++) {
+    rectangle(
+      '#444444',
+      snakeSize * i,
+      snakeSize * j,
+      snakeSize - 1,
+      snakeSize - 1)
+    };
+  };
+
+};
+
+// reset food
+const resetFood = () => {
+  foodPosX = Math.floor(Math.random() * tileCountX) * snakeSize;
+  foodPosY = Math.floor(Math.random() * tileCountY) * snakeSize;
+};
+
 // ** KEYBOARD
 const keyPush = (event) => {
 
   switch (event.key) {
     case 'ArrowLeft':
-      velocityX = -1;
-      velocityY = 0;
+      if (velocityX !== 1) {
+        velocityX = -1;
+        velocityY = 0;
+      }
       break;
 
     case 'ArrowUp':
-      velocityX = 0;
-      velocityY = -1;
+      if (velocityY !== 1) {
+        velocityX = 0;
+        velocityY = -1;
+      }
       break;
 
     case 'ArrowRight':
-      velocityX = 1;
-      velocityY = 0;
+      if (velocityX !== -1) {
+        velocityX = 1;
+        velocityY = 0;
+      }
       break;
           
     case 'ArrowDown':
-      velocityX = 0;
-      velocityY = 1;
+      if (velocityY !== -1) {
+        velocityX = 0;
+        velocityY = 1;
+      }
       break;
   };
 };
@@ -94,4 +147,5 @@ const keyPush = (event) => {
 // listeners
 document.addEventListener('keydown', keyPush);
 
+resetFood();
 gameLoop();
